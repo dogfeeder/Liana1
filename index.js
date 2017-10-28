f897e4d18e78d282b1e441aa8c4550d3379d7b8e
Patrick Freeman <patrick@lup.com.au>
2017-10-28 21:47:15 +1000

    Got some things moving...

--- index.js
+++ index.js
@@ -1,14 +1,58 @@
+console.log("Loading three.js scene...")
 var scene = new THREE.Scene();
 var camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
-
+var objectsRight = [];
+var objectsLeft = [];
 var renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
+renderer.setClearColor(0xffffff, 0);
 document.body.appendChild(renderer.domElement);
-
-var geometry = new THREE.BoxGeometry(1, 1, 1);
-var material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
-var cube = new THREE.Mesh(geometry, material);
-scene.add(cube);
+var isHeart = false;
 
 camera.position.z = 5;
-console.log(scene);
+
+function spawnAvo(direction, x, y) {
+    var spriteMap = isHeart ?
+        new THREE.TextureLoader().load("images/avocado.png") :
+        new THREE.TextureLoader().load("images/heart.png");
+
+    var spriteMaterial = new THREE.SpriteMaterial({ map: spriteMap, color: 0xffffff });
+    var sprite = new THREE.Sprite(spriteMaterial);
+    sprite.scale.set(0.5, 0.5, 0.5);
+    sprite.position.set(x, y, 0);
+    scene.add(sprite);
+
+    if (direction === "right") {
+        objectsRight.push(sprite);
+    } else {
+        objectsLeft.push(sprite);
+    }
+}
+
+
+function startSpawning() {
+
+    row1 = setInterval(function () {
+        spawnAvo("right", -6, 3);
+    }, 400);
+
+    row2 = setInterval(function () {
+        spawnAvo("left", 6, -3);
+        isHeart = !isHeart;
+    }, 400);
+}
+
+function animate() {
+    for (var i = 0; i < objectsRight.length; i++) {
+        objectsRight[i].position.x += 0.03;
+    }
+
+    for (var i = 0; i < objectsLeft.length; i++) {
+        objectsLeft[i].position.x -= 0.03;
+    }
+    requestAnimationFrame(animate);
+    renderer.render(scene, camera);
+}
+
+animate();
+startSpawning();
